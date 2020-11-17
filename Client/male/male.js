1f093d13c92c57aacb216a4725b207b09d883ad2
mew <33283117+mew18@users.noreply.github.com>
2020-11-17 23:14:26 +0530

    Update male.js

--- Client/male/male.js
+++ Client/male/male.js
@@ -1,31 +1,29 @@
-function display(url) {
-    var req = new XMLHttpRequest();
-    req.open("GET", url, true);
-    req.responseType = 'document';
-    
-    document.write("before if")
-    
-    req.onload = () => {
-        if (req.status == 200) {
-            var elements = req.response.getElementsByTagName("a");
-            for (x of elements) {
-                if (x.href.match(/\.(jpg)$/)) {
-                    let img = document.createElement("img");
-                    img.src = x.href;
+var findImages = function(){
+    var parentDir = "../../Classified_Images/male/";
 
-                    let div = document.createElement('div');
-                    div.className = "female";
-                    div.innerHTML = img.src
-                    document.body.append(img);
-                    // document.body.button.appendChild(img);
-                }
-            };
-        }
-        else {
-            alert('Request failed lol fuck.' + req.status);
-        }
-    }
-    req.send()
+    var fileCrowler = function(data){
+        var titlestr = $(data).filter('title').text();
+        // "Directory listing for /Resource/materials/xxx"
+        var thisDirectory = titlestr.slice(titlestr.indexOf('/'), titlestr.length)
+
+        //List all image file names in the page
+        $(data).find("a").attr("href", function (i, filename) {
+            if( filename.match(/\.(jpe?g|png|gif)$/) ) { 
+                var fileNameWOExtension = filename.slice(0, filename.lastIndexOf('.'))
+                var img_html = "<img src='{0}' id='{1}' alt='{2}' width='75' height='75' hspace='2' vspace='2' onclick='onImageSelection(this);'>".format(thisDirectory + filename, fileNameWOExtension, fileNameWOExtension);
+                $("#image_pane").append(img_html);
+            }
+            else{ 
+                $.ajax({
+                    url: thisDirectory + filename,
+                    success: fileCrowler
+                });
+            }
+        });}
+
+        $.ajax({
+        url: parentDir,
+        success: fileCrowler
+    });
 }
-
-display("../../Classified_Images/female/")
+// display("../../Classified_Images/male/")
