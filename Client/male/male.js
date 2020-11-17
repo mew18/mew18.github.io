173a94ac952c06e4fb3c89b850b65346cd20178b
mew <33283117+mew18@users.noreply.github.com>
2020-11-17 22:01:05 +0530

    Add files via upload

--- Client/male/male.js
+++ Client/male/male.js
@@ -1,14 +1,19 @@
 function display(url)
 {
     var req = new XMLHttpRequest();
-    req.open("GET",url, true);
+    console.log("1");
+    req.open("GET", url, true);
+    console.log("1.5");
     req.responseType = 'document';
 
     req.onload = () => {
+        console.log("2");
         if (req.status === 200) {
+            console.log("3");
             var elements = req.response.getElementsByTagName("a");
             for (x of elements) {
                 if (x.href.match(/\.(jpg)$/)) {
+                    console.log("4");
                     let img = document.createElement("img");
                     img.src = x.href;
                     document.body.appendChild(img);
@@ -16,6 +21,7 @@ function display(url)
             };
         }
         else {
+            console.log("5");
             alert('Request failed.' + req.status);
         }
     }
