0e57becd01f7e8da77e9827d87b2dab35826bdcc
mew <33283117+mew18@users.noreply.github.com>
2020-11-17 23:18:08 +0530

    Update male.js

--- Client/male/male.js
+++ Client/male/male.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
   var dir = "../../Classified_Images/male/"; // folder location
   var fileextension = ".jpg"; // image format
-  var i = "1";
+  var i = "0";
 
   $(function imageloop(){
-    $("<img />").attr('src', dir + i + fileextension ).appendTo(".testing");
-    if (i==13){
+    $("<img />").attr('src', dir + "img_"+ i + fileextension ).appendTo(".testing");
+    if (i==15){
       alert('loaded');
     }
     else{
