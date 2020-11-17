a647c935d159b1267d4501717eb938157a036f4b
mew <33283117+mew18@users.noreply.github.com>
2020-11-18 02:28:20 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var folder = "./Classified_Images/male";
+console.log("ok");
+$.ajax({
+    url: folder,
+    success: function (data) {
+        $(data).find("a").attr("href", function (i, val) {
+            if (val.match(/\.(jpg)/)) {
+                $("body").append("<img src='" + val + "'>");
+            }
+        });
+    }
+});
