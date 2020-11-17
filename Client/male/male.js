249931d3a27fd86635201397dce2355fd4f44540
mew <33283117+mew18@users.noreply.github.com>
2020-11-17 22:14:59 +0530

    Add files via upload

--- Client/male/male.js
+++ Client/male/male.js
@@ -1,31 +1,27 @@
-function display(url)
-{
-    var req = new XMLHttpRequest();
-    console.log("1");
-    req.open("GET", url, true);
-    console.log("1.5");
-    req.responseType = 'document';
+var req = new XMLHttpRequest();
+document.write("1");
+req.open("GET", "../../Classified_Images/male", false);
+document.write("1.5");
+req.responseType = 'document';
 
-    req.onload = () => {
-        console.log("2");
-        if (req.status === 200) {
-            console.log("3");
-            var elements = req.response.getElementsByTagName("a");
-            for (x of elements) {
-                if (x.href.match(/\.(jpg)$/)) {
-                    console.log("4");
-                    let img = document.createElement("img");
-                    img.src = x.href;
-                    document.body.appendChild(img);
-                }
-            };
-        }
-        else {
-            console.log("5");
-            alert('Request failed.' + req.status);
-        }
+req.onload = () => {
+    document.write("2");
+    if (req.status === 200) {
+        document.write("3");
+        var elements = req.response.getElementsByTagName("a");
+        for (x of elements) {
+            if (x.href.match(/\.(jpg)$/)) {
+                document.write("4");
+                let img = document.createElement("img");
+                img.src = x.href;
+                document.body.appendChild(img);
+            }
+        };
     }
-    req.send()
+    else {
+        document.write("5");
+        alert('Request failed.' + req.status);
+    }
 }
+req.send()
 
-display("../../Classified_Images/male")
