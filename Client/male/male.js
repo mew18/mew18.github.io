ec853728c2e0c6384cbefe3fd2ee0cc694b93e86
mew <33283117+mew18@users.noreply.github.com>
2020-11-17 23:15:56 +0530

    Update male.js

--- Client/male/male.js
+++ Client/male/male.js
@@ -1,29 +1,17 @@
-var findImages = function(){
-    var parentDir = "../../Classified_Images/male/";
+$(document).ready(function(){
+  var dir = "../../Classified_Images/male/"; // folder location
+  var fileextension = ".jpg"; // image format
+  var i = "1";
 
-    var fileCrowler = function(data){
-        var titlestr = $(data).filter('title').text();
-        // "Directory listing for /Resource/materials/xxx"
-        var thisDirectory = titlestr.slice(titlestr.indexOf('/'), titlestr.length)
-
-        //List all image file names in the page
-        $(data).find("a").attr("href", function (i, filename) {
-            if( filename.match(/\.(jpe?g|png|gif)$/) ) { 
-                var fileNameWOExtension = filename.slice(0, filename.lastIndexOf('.'))
-                var img_html = "<img src='{0}' id='{1}' alt='{2}' width='75' height='75' hspace='2' vspace='2' onclick='onImageSelection(this);'>".format(thisDirectory + filename, fileNameWOExtension, fileNameWOExtension);
-                $("#image_pane").append(img_html);
-            }
-            else{ 
-                $.ajax({
-                    url: thisDirectory + filename,
-                    success: fileCrowler
-                });
-            }
-        });}
-
-        $.ajax({
-        url: parentDir,
-        success: fileCrowler
-    });
-}
+  $(function imageloop(){
+    $("<img />").attr('src', dir + i + fileextension ).appendTo(".testing");
+    if (i==13){
+      alert('loaded');
+    }
+    else{
+      i++;
+      imageloop();
+    };
+  });   
+});
 // display("../../Classified_Images/male/")
