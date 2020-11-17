b8c1e42763ebc75e3795575ff33884de60200cf7
mew <33283117+mew18@users.noreply.github.com>
2020-11-18 02:39:43 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
-var folder = "./Classified_Images/male/";
-console.log("ok");
+var folder = "Classified_Images/male/";
+console.log("ok1");
 $.ajax({
     url: folder,
     success: function (data) {
         $(data).find("a").attr("href", function (i, val) {
+            console.log("ok2");
             if (val.match(/\.(jpg)/)) {
+                console.log("ok3");
                 $("body").append("<img src='" + val + "'>");
+                console.log("ok4");
             }
         });
     }
